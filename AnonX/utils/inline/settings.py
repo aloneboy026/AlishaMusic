f91f1e5e256b3171dcc6b8e884fb143cb06f7ef0
aloneboy026 <122468535+aloneboy026@users.noreply.github.com>
2023-02-22 19:08:19 +0530

    Update settings.py

--- AnonX/utils/inline/settings.py
+++ AnonX/utils/inline/settings.py
@@ -8,18 +8,18 @@ def setting_markup(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="ᴀᴜᴅɪᴏ ǫᴜᴀʟɪᴛʏ", callback_data="AQ"
+                text="🚶 ᴀᴜᴅɪᴏ ǫᴜᴀʟɪᴛʏ 🚶", callback_data="AQ"
             ),
             InlineKeyboardButton(
-                text="ᴠɪᴅᴇᴏ ǫᴜᴀʟɪᴛʏ", callback_data="VQ"
+                text="🦔 ᴠɪᴅᴇᴏ ǫᴜᴀʟɪᴛʏ 🦔", callback_data="VQ"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="ᴀᴜᴛʜ ᴜsᴇʀs", callback_data="AU"
+                text="🍄 ᴀᴜᴛʜ ᴜsᴇʀs 🍄", callback_data="AU"
             ),
             InlineKeyboardButton(
-                text="ᴄʟᴇᴀɴ ᴍᴏᴅᴇ", callback_data="CM"
+                text="🐦 ᴄʟᴇᴀɴ ᴍᴏᴅᴇ 🐦", callback_data="CM"
             ),
         ],
         [
