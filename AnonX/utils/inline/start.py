c57f9b9773dc2ecd4e4d03c0fffc460c90d13f05
aloneboy026 <122468535+aloneboy026@users.noreply.github.com>
2023-02-22 19:00:50 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -38,7 +38,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🥺 ᴀᴅᴅ ᴍᴇ ᴇʟsᴇ ʏᴏᴜ ɢᴇʏ 🥺",
+                text="🥺 ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ʏᴏᴜ ɢᴇʏ 🥺",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             ),
         ],
@@ -55,7 +55,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                    text="sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ", url=f"{config.UPSTREAM_REPO}"
+                    text="sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ", url=f"{https://te.legra.ph/file/ddcb156251c1624d09253.mp4}"
                 )
         ],
      ]
